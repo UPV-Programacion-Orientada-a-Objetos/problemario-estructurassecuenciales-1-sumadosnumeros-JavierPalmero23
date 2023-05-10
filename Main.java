c75e74f95e8025313c30524170a79f638ce37b3e
JavierPalmero23 <rengerjavier@gmail.com>
2023-05-10 12:07:09 -0600

    Modificaciones Problemario java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-class Main {
 
+class Main{
     public static void main(String[] args) {
-
+	SumaDosNumeros suma = new SumaDosNumeros();
+	System.out.println(suma.suma(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
     }
 }
